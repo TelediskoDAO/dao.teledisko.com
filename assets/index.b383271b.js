6c805b02c207a42917a7cf1772b70df575ad2fd3
stecb <stefano.ceschib@gmail.com>
2023-01-13 17:02:53 +0000

    Deploying to main from @ TelediskoDAO/dapp@39daa78c7ed35cf0236c8b27de267eec35f08221 🚀

--- assets/index.b383271b.js
+++ assets/index.b383271b.js
@@ -1,4 +1,4 @@
-import{S as q,M as H,l as S,v as U,$ as W,b as B,B as Q,f as N}from"./index.c2b6cb27.js";import{d as or}from"./dijkstra.c6eae3dd.js";/**
+import{S as q,M as H,l as S,v as U,$ as W,b as B,B as Q,f as N}from"./index.a5cb7900.js";import{d as or}from"./dijkstra.c6eae3dd.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
